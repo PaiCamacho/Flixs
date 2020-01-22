89d08aad867394e0d04eb4ae34453269304ec8b5
Paola <camachopaola311@gmail.com>
2020-01-22 18:45:40 -0500

    Initial Commit

---
+++ Flixs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixs
+//
+//  Created by Paola Camacho on 1/22/20.
+//  Copyright © 2020 Paola Camacho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
