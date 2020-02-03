33b4edd0b71985c2069767f08433591072550dc8
Paola <camachopaola311@gmail.com>
2020-02-02 22:28:41 -0500

    Added cell to view details on movies and a tab bar to view Movie Posters

---
+++ Flixs/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixs
+//
+//  Created by Paola Camacho on 2/2/20.
+//  Copyright © 2020 Paola Camacho. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixs/MoviesViewController.swift
+++ Flixs/MoviesViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AlamofireImage
 
 class MoviesViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -29,25 +29,25 @@ class MoviesViewController : UIViewController, UITableViewDataSource, UITableVie
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
-           // This will run when the network request returns
-           if let error = error {
-              print(error.localizedDescription)
-           } else if let data = data {
-              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-            
-              self.movies = dataDictionary["results"] as! [[String:Any]]
-              self.tableView.reloadData()
-              print(dataDictionary)
-            
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
-              // TODO: Get the array of movies
-              // TODO: Store the movies in a property to use elsewhere
-              // TODO: Reload your table view data
-
-           }
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                self.tableView.reloadData()
+                
+                
+                
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
         }
         task.resume()
-
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
@@ -71,6 +71,16 @@ class MoviesViewController : UIViewController, UITableViewDataSource, UITableVie
         cell.posterView.af_setImage(withURL: posterUrl)
         return cell
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
